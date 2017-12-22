bcbf518c625f568707503c25c148e4dfc3849b77
NUmeroAndDev <numero.dev.and@gmail.com>
2017-12-22 11:09:25 +0900

    FragmentとAdapterの追加

---
+++ app/src/main/java/com/numero/colorpalette/View/Adapter/PagerAdapter.java
@@ -0,0 +1,22 @@
+package com.numero.colorpalette.View.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/numero/colorpalette/fragment/ColorListFragment.java
@@ -0,0 +1,7 @@
+package com.numero.colorpalette.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class ColorListFragment extends Fragment {
+
+}
