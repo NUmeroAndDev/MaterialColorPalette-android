47feff40f6b10c70c23fe6265286c5d1b5bde4e2
NUmeroAndDev <numero.dev.and@gmail.com>
2017-12-22 12:30:33 +0900

    FramgentにViewの追加

--- app/src/main/java/com/numero/colorpalette/fragment/ColorListFragment.java
+++ app/src/main/java/com/numero/colorpalette/fragment/ColorListFragment.java
@@ -10,10 +10,26 @@ import android.view.ViewGroup;
 
 import com.numero.colorpalette.R;
 import com.numero.colorpalette.model.color.MaterialColor;
+import com.numero.colorpalette.view.ColorStateView;
 
 public class ColorListFragment extends Fragment {
 
     private static final String BUNDLE_MATERIAL_COLOR = "MATERIAL_COLOR";
+
+    private ColorStateView colorState50View;
+    private ColorStateView colorState100View;
+    private ColorStateView colorState200View;
+    private ColorStateView colorState300View;
+    private ColorStateView colorState400View;
+    private ColorStateView colorState500View;
+    private ColorStateView colorState600View;
+    private ColorStateView colorState700View;
+    private ColorStateView colorState800View;
+    private ColorStateView colorState900View;
+    private ColorStateView colorStateA100View;
+    private ColorStateView colorStateA200View;
+    private ColorStateView colorStateA400View;
+    private ColorStateView colorStateA700View;
 
     private MaterialColor materialColor;
 
@@ -42,6 +58,19 @@ public class ColorListFragment extends Fragment {
     }
 
     private void initViews(@NonNull View view) {
-
+        colorState50View = view.findViewById(R.id.color_state_50_view);
+        colorState100View = view.findViewById(R.id.color_state_100_view);
+        colorState200View = view.findViewById(R.id.color_state_200_view);
+        colorState300View = view.findViewById(R.id.color_state_300_view);
+        colorState400View = view.findViewById(R.id.color_state_400_view);
+        colorState500View = view.findViewById(R.id.color_state_500_view);
+        colorState600View = view.findViewById(R.id.color_state_600_view);
+        colorState700View = view.findViewById(R.id.color_state_700_view);
+        colorState800View = view.findViewById(R.id.color_state_800_view);
+        colorState900View = view.findViewById(R.id.color_state_900_view);
+        colorStateA100View = view.findViewById(R.id.color_state_a100_view);
+        colorStateA200View = view.findViewById(R.id.color_state_a200_view);
+        colorStateA400View = view.findViewById(R.id.color_state_a400_view);
+        colorStateA700View = view.findViewById(R.id.color_state_a700_view);
     }
 }
