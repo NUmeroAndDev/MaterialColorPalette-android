3ad415cc5541e59dc043123c5e967854ade66258
NUmeroAndDev <numero.dev.and@gmail.com>
2017-12-22 11:29:36 +0900

    Activityの変更

---
+++ app/src/main/java/com/numero/colorpalette/activity/MainActivity.java
@@ -0,0 +1,53 @@
+package com.numero.colorpalette.activity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.numero.colorpalette.R;
+import com.numero.colorpalette.view.adapter.PagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        pagerAdapter = new PagerAdapter(getApplicationContext(), getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
